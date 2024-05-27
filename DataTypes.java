87a245fb623ea666b218c08a5f9b97dda12faada
ASUS <hirbad.he@gmial.com>
2024-05-27 23:10:02 +0330

    Not Needed

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Info");
+
+        String name="Mohammad";
+        String family="Hedayati";
+        int age=40;
+        int hight=181;
+        long salary=10000000;
+        double bankACC=1234.5678;
+        boolean married=false;
+
+
+        System.out.println("NAME: "+name);
+        System.out.println("FAMILY: "+family);
+        System.out.println("AGE: "+age);
+        System.out.println("HIGHT: "+hight);
+        System.out.println("SALARY: "+salary);
+        System.out.println("BANK_ACC: "+bankACC);
+        System.out.println("MARRIED: "+married);
+    }
+}
